115276e22f341435d3a6f0d36935ee8001b1194e
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2023-10-10 17:56:39 +0330

    Create Plot_Loss.py

---
+++ New folder/Functions/Plot_Loss.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+# ====================================== Plot loss for train & test ==================================================   
+def plot_loss(train_model, axs, title=None):
+    # _, axs = plt.subplots(nrows=1, sharey='row', figsize=(4, 3))
+    axs.plot(train_model.history["loss"], label='training loss')
+    axs.plot(train_model.history["val_loss"], label='validation loss',)
+    axs.legend(fontsize=10, ncol=1, loc='best', labelcolor='linecolor', handlelength=0)
+    axs.autoscale(enable=True, axis="x",tight=True)
+    axs.set_xlabel('Epochs', fontsize=10)
+    axs.set_title(title, fontsize=10)
