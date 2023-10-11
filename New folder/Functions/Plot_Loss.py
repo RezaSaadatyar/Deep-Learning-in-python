afad86b37d844ce3a081a76b4daa134f569bad24
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2023-10-11 23:53:44 +0330

    Update Plot_Loss.py

--- New folder/Functions/Plot_Loss.py
+++ New folder/Functions/Plot_Loss.py
@@ -9,3 +9,6 @@ def plot_loss(train_model, axs, title=None):
     axs.autoscale(enable=True, axis="x",tight=True)
     axs.set_xlabel('Epochs', fontsize=10)
     axs.set_title(title, fontsize=10)
+    plt.gca().yaxis.set_major_formatter('{:.2f}'.format)
+    axs.grid(axis='y', linestyle='--', alpha=0.25)
+    plt.subplots_adjust(wspace=0.035, hspace=0.2)
