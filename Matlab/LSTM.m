89d73ef816b519a57ba9c42fc386c8e9edb43573
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-05-21 14:32:14 +0330

    app

--- Matlab/LSTM.m
+++ Matlab/LSTM.m
@@ -1,2 +1,23 @@
 clc; clear; close all;
+%% ------------------------------------ Load data ------------------------------------
+data = chickenpox_dataset;
+data = [data{:}];
+%% --------------------------
+trec = numel(data);
+trrec = 0.9 * trec;
+NTST = floor(trrec);
+datatrain = data(1:NTST + 1);
+datatest = data(NTST +1 : end);
+mu = mean(datatrain);
+sig = std(datatrain);
+datatrainstd = (datatrain - mu) / sig;
+xtrain = datatrainstd(1:end-1);
+ytrain = datatrainstd(2:end);
 
+
+%% -------------------------------------- Plot ---------------------------------------
+figure;
+plot(data)
+xlabel("Months")
+ylabel("Cases")
+title("Monthly cases of chickenpox")
