ec8944f0836c3788ab891c0ca3a8c7f6bed18412
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-05-22 19:14:29 +0330

    app

--- Matlab/LSTM.m
+++ Matlab/LSTM.m
@@ -24,10 +24,31 @@ fullyConnectedLayer(NOR, "Name", "FC")
 regressionLayer("Name", "RL")];
 % lgraph=layerGraph(layers);
 % plot (lgraph)
-
+options = trainingOptions("adam",...
+    "MaxEpochs", 250, ...
+    "GradientThreshold", 1,...
+    "InitialLearnRate", 0.005,...
+    "LearnRateSchedule", "piecewise",...
+    "LearnRateDropPeriod", 125, ...
+    "LearnRateDropFactor", 0.2,...
+    "Verbose", 0,...
+    "Plots", "training-progress");
+net = trainNetwork(xtrain , ytrain, layers, options);
+datateststd = (datatest - mu) / sig;
+xtest = datateststd(1:end-1);
+ytest = datatest(2:end);
+net = predictAndUpdateState(net, xtrain);
+[net, ypred] = predictAndUpdateState(net, ytrain(end));
+NTSTs = numel(datatest);
+for i = 2:NTSTs
+    [net, ypred(:, i)] = predictAndUpdateState(net, ytrain(:, i-1));
+end
+ypred = sig*ypred + mu;
+rmse =sqrt(mean(ypred(2:end) - ytest).^2);
 %% -------------------------------------- Plot ---------------------------------------
 figure;
-plot(data)
+plot(data); hold on;
+plot(NTST:NTST + NTSTs, [data(NTST) ypred], '.-')
 xlabel("Months")
 ylabel("Cases")
 title("Monthly cases of chickenpox")
